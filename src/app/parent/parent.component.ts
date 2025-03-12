feec39f979abde846f22d098374df97ee56831ca
savritshriffle <savrit@shriffle.com>
2025-03-12 17:45:34 +0530

    oprators

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,13 +1,17 @@
-import { Component, ContentChild, ViewChild, AfterContentInit, ElementRef, OnInit} from '@angular/core';
+import { Component, ContentChild, ViewChild, AfterContentInit, ElementRef, OnInit } from '@angular/core';
 import { ChildComponent } from '../child/child.component';
 import { ActivatedRoute } from '@angular/router';
+import { of, forkJoin, from, Observable, throwError, map, filter, mergeMap, find, pipe, first, last } from 'rxjs';
+import { NumberValueAccessor } from '@angular/forms';
+import { __values } from 'tslib';
+
 
 @Component({
     selector: 'app-parent',
     templateUrl: './parent.component.html',
     styleUrls: ['./parent.component.css']
 })
-export class ParentComponent  implements  AfterContentInit, OnInit{
+export class ParentComponent  implements  AfterContentInit, OnInit {
 
     constructor(private route : ActivatedRoute) { }
 
@@ -19,6 +23,29 @@ export class ParentComponent  implements  AfterContentInit, OnInit{
     Data: string = '';
     title: string = '';
     massage: string = '';
+    oprator = of([1,2,3,4],"Hello", "User", {
+        name : 'firstName',
+        age: 10,
+    }
+    )
+    fromOprator = from([1,2,3,4,5,6,7,8,"hello" ,{
+        name: "firstName",
+        age: 10
+    }])
+
+    array = "string"
+    fromExample = from(this.array)
+    var1 = {
+        name : "firstName",
+        age : 10
+    }
+    var2 = [9,'6',7,0]
+    var3 = [11, 12, 13]
+    forkJoinExample = forkJoin(this.var1.name , this.var2, )
+    throwerr = throwError(["Internal error"])
+    margeMapExample = of([1,2,3,4,5])
+    margeMapExample2 = of('a', 'b', 'c', 'd')
+    margeMapExample3 = of('1', '2', '3', '4')
     @ViewChild(ChildComponent) childel!: ChildComponent
     childdata(){
         this.childel.datachild("data change by parent component")
@@ -64,9 +91,83 @@ export class ParentComponent  implements  AfterContentInit, OnInit{
         this.title = this.route.snapshot.queryParams['title']
         this.massage = this.route.snapshot.queryParams['massage']
         console.log(this.route.snapshot.queryParams)
-        console.log(this.route.snapshot.fragment)
+        console.log(this.route.snapshot.fragment)   
         
-    }
+        // this.obj.subscribe((value)=>{
+        //      console.log(value)
+           
+        // })
+        
+        this.oprator.subscribe((value:any)=>{
+            console.log(value.age)
+            console.log(value.name)
+            console.log()
+        })
+        this.oprator.pipe(
+            // first()
+            last()
+        ).subscribe((value)=>{
+            console.log(value)
+        })
+
+        
+
+        this.fromOprator.pipe(first()).subscribe((value)=>{
+            console.log(value)
+        })
+    //     this.fromExample.subscribe((value)=>{
+    //         console.log(value)
+    //     })
+    //     this.forkJoinExample.subscribe((value)=>{
+    //         console.log(value)
+    //     })
+            this.throwerr.subscribe()
+
+        this.margeMapExample.pipe(
+            mergeMap(val => {
+                console.log(val)
+                return this.margeMapExample2
+            })
+        ).subscribe((value)=>{
+            console.log(value)
+        })
+    // }
+    // obj = new Observable((observer)=>{
+    //     console.log("Observable.")
+    //     let value = [1,2,3,4,5,6,7,8,9,0]
+    //     observer.next(value)
+    //     setTimeout(()=>{
+    //         observer.next('a')
+    //     },5000)
+    //     setTimeout(()=>{
+    //         observer.next('b')
+    //     },4000)
+    //     // observer.next('b')
+    //     // observer.next('d')
+    //     setTimeout(()=>{
+    //         observer.complete()
+    //         console.log("Task is completed ")    
+    //     },4500)
+    //     console.log(observer.unsubscribe)
+        // observer.error()
+        
+        // value.filter((val)=>{
+        //     if(val<=5){
+        //         console.log("task is completed = "+val)
+        //         observer.complete()
+        //     }
+        // })
+    // })
+    
+    
+    // Observ(){
+    //    this.obj.subscribe((value)=>{
+    //         console.log(value)
+           
+    //     })
+
+   // }
+  
 
 }
-
+}
